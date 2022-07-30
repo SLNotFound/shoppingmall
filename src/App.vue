cf20fcb5c1243ff2fda5ed5773abf60fa87d1d4b
sunl <sunlight0915@163.com>
2022-07-30 17:31:08 +0800

    修改主页

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,27 @@
 <template>
-  <div id="app">123</div>
+  <div class="docker">
+    <div class="docker__item">首页</div>
+    <div class="docker__item">购物车</div>
+    <div class="docker__item">订单</div>
+    <div class="docker__item">我的</div>
+  </div>
 </template>
 
 <style lang="scss">
-
+  .docker {
+    display: flex;
+    box-sizing: border-box;
+    position: absolute;
+    padding: 0 .18rem;
+    left: 0;
+    bottom: 0;
+    width: 100%;
+    height: .49rem;
+    border-top: 1px solid #F1F1F1;
+    background: green;
+  }
+  .docker__item {
+    flex: 1;
+    text-align: center;
+  }
 </style>
